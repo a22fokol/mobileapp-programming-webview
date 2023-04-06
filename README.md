9fed9d098b66e137a90d38967c7ee58b35af76c9
a22fokol <a22fokol@student.his.se>
2023-04-06 10:04:18 +0200

    Jag är färdig med rapporten

--- README.md
+++ README.md
@@ -1,39 +1,30 @@
 
 # Rapport
+Jag skapade html som heter "Bonjour" en internal link som är kallad in i "showInternalWebPage" 
+och det externalwebpage soom är kallad in i "showInternalWebPage"
 
-**Skriv din rapport här!**
-
-_Du kan ta bort all text som finns sedan tidigare_.
+_
 
 ## Följande grundsyn gäller dugga-svar:
 
-- Ett kortfattat svar är att föredra. Svar som är längre än en sida text (skärmdumpar och programkod exkluderat) är onödigt långt.
-- Svaret skall ha minst en snutt programkod.
-- Svaret skall inkludera en kort övergripande förklarande text som redogör för vad respektive snutt programkod gör eller som svarar på annan teorifråga.
-- Svaret skall ha minst en skärmdump. Skärmdumpar skall illustrera exekvering av relevant programkod. Eventuell text i skärmdumpar måste vara läsbar.
-- I de fall detta efterfrågas, dela upp delar av ditt svar i för- och nackdelar. Dina för- respektive nackdelar skall vara i form av punktlistor med kortare stycken (3-4 meningar).
-
-Programkod ska se ut som exemplet nedan. Koden måste vara korrekt indenterad då den blir lättare att läsa vilket gör det lättare att hitta syntaktiska fel.
 
 ```
-function errorCallback(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            // Geolocation API stöds inte, gör något
-            break;
-        case error.POSITION_UNAVAILABLE:
-            // Misslyckat positionsanrop, gör något
-            break;
-        case error.UNKNOWN_ERROR:
-            // Okänt fel, gör något
-            break;
+public void showExternalWebPage(){
+        myWebWiew.loadUrl("https://www.youtube.com/watch?v=YA_sNH6wFA0");
+        // TODO: Add your code for showing external web page here
     }
-}
+
+    public void showInternalWebPage(){
+        myWebWiew.loadUrl("file:///android_asset/Bonjour.html");
+        // TODO: Add your code for showing internal web page here
+
+    }
 ```
 
 Bilder läggs i samma mapp som markdown-filen.
 
-![](android.png)
+![](Screenshot_20230406_094221.png)
+![](Screenshot_20230406_095043.png)
 
 Läs gärna:
 
--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,44 +13,30 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     public void showExternalWebPage(){
+        myWebWiew.loadUrl("https://www.youtube.com/watch?v=YA_sNH6wFA0");
         // TODO: Add your code for showing external web page here
     }
 
     public void showInternalWebPage(){
+        myWebWiew.loadUrl("file:///android_asset/Bonjour.html");
         // TODO: Add your code for showing internal web page here
+
     }
 
+    private WebView myWebWiew;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        myWebWiew=(WebView) findViewById(R.id.my_webview);
+        myWebWiew.setWebViewClient(new WebViewClient());
+        myWebWiew.getSettings().setJavaScriptEnabled(true);
 
-        /*
-        * Rename your App. Tip: Values->Strings
-        * Enable Internet access for your App. Tip: Manifest
-        * Create a WebView element in the layout file main_activity.xml
-        * Give the WebView element ID "my_webview"
-        -- Commit and push to your github fork
-        * Create a private member variable called "myWebView" of type WebView
-        * Locate the WebView element created in step 1 using the ID created in step 2
-        * Create a new WebViewClient to attach to our WebView. This allows us to
-          browse the web inside our app.
-        -- Commit and push to your github fork
-        * Enable Javascript execution in your WebViewClient
-        * Enter the url to load in our WebView
-        -- Commit and push to your github fork
-        * Move the code that loads a URL into your WebView into the two methods
-          "showExternalWebPage()" and "showInternalWebPage()".
-        * Call the "showExternalWebPage()" / "showInternalWebPage()" methods
-          when you select menu options "External Web Page" or "Internal Web Page"
-          respectively
-        -- Commit and push to your github fork
-        * Take two screenshots using the "Take a screenshot" tool in the AVD
-           showing your App. One (1) screenshot showing your internal web page and
-           one (1) screenshot showing your external web page.
-        */
+
+
     }
 
     @Override
@@ -67,12 +55,14 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
-            Log.d("==>","Will display external web page");
+            showExternalWebPage();
+            Log.d("==>","showExternalWebPage()");
             return true;
         }
 
         if (id == R.id.action_internal_web) {
-            Log.d("==>","Will display internal web page");
+            showInternalWebPage();
+            Log.d("==>","showInternalWebPage()");
             return true;
         }
 
